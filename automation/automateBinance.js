f55db1f987f048fad64370721fdfdc419cd6b24c
Mahbub Rahman <lover.mahabub@gmail.com>
2023-12-08 16:49:21 +0600

    add error catch on cron schedule

--- automation/automateBinance.js
+++ automation/automateBinance.js
@@ -61,7 +61,7 @@ const sendMessageToFbUser = async(currentLowestPrice) => {
     console.log(`SendMessageToFbUser`, getUpdates.data);
 }
 const checkIntervalMinute = CHECK_INTERVAL || 3;
-cron.schedule(`${checkIntervalMinute} * * * *`, () => {
+cron.schedule(`* * * * *`, () => {
     try {
         currentP2Pprices();
     } catch (error) {
