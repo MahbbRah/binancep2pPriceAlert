c295799284cc0cb9baec05a06fdff4b3781f3dc1
Mahbub Rahman <lover.mahabub@gmail.com>
2023-12-08 17:15:03 +0600

    somewhat the cron not working as expected

--- automation/automateBinance.js
+++ automation/automateBinance.js
@@ -61,7 +61,7 @@ const sendMessageToFbUser = async(currentLowestPrice) => {
     console.log(`SendMessageToFbUser`, getUpdates.data);
 }
 const checkIntervalMinute = CHECK_INTERVAL || 3;
-cron.schedule(`* ${checkIntervalMinute} * * * *`, () => {
+cron.schedule(`* 3 * * * *`, () => {
     try {
         currentP2Pprices();
     } catch (error) {
