297f12be1a1633ce22c6cee6c95dd705b8acfe96
Mahbub Rahman <lover.mahabub@gmail.com>
2023-12-08 16:47:00 +0600

    update app to take static value from env

--- automation/automateBinance.js
+++ automation/automateBinance.js
@@ -2,7 +2,15 @@ require('dotenv').config();
 const cron = require('node-cron');
 const axios = require("axios").default;
 
-const MINIMUM_PRICE = parseFloat(1.011);
+const { 
+    MINIMUM_PRICE, 
+    PSID, 
+    PAGE_ID, 
+    CHECK_INTERVAL, 
+    ACCESS_TOKEN 
+} = process.env;
+const MIN_PRICE = parseFloat(MINIMUM_PRICE);
+
 const currentP2Pprices = async() => {
     const reqUri = `https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search`;
     const payload = {
@@ -24,18 +32,16 @@ const currentP2Pprices = async() => {
     const firstPrice = parseFloat(getmarketPrices[0]);
     getmarketPrices = getmarketPrices.join(',');
     // if minimum price is higher or equal to first price
-    if (MINIMUM_PRICE >= firstPrice) {
+    if (MIN_PRICE >= firstPrice) {
         await sendMessageToFbUser(getmarketPrices)
     }
     console.log('marketPrices:', getmarketPrices); // price example: 1.015
 }
 
 const sendMessageToFbUser = async(currentLowestPrice) => {
-    const page_id = `102770435320525`;
-    const reqUri = `https://graph.facebook.com/v18.0/${page_id}/messages`;
+    const reqUri = `https://graph.facebook.com/${GRAPH_VERSION}/${PAGE_ID}/messages`;
     
     const messageBody = `Lowest price for first 3 advertisers: ${currentLowestPrice}`;
-    const PSID = "6988520177867851"; // page scoped user ID
     const payload = {
         "messaging_type": "RESPONSE",
         "recipient": {
@@ -48,11 +54,11 @@ const sendMessageToFbUser = async(currentLowestPrice) => {
 
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+        'Authorization': `Bearer ${ACCESS_TOKEN}`
     }
 
     const getUpdates =  await axios.post(reqUri, payload, { headers });
     console.log(`SendMessageToFbUser`, getUpdates.data);
 }
-
-cron.schedule('3 * * * *', currentP2Pprices);
+const checkIntervalMinute = CHECK_INTERVAL || 3;
+cron.schedule(`${checkIntervalMinute} * * * *`, currentP2Pprices);
